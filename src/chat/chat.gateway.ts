59ae74baa5f0d77104b885e72baff8164dba1115
VladZawinski <shahadzawinski.non@gmail.com>
2024-02-14 11:18:43 +0630

    Sent to token

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -75,7 +75,10 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection {
             const element = subscribers[i];
             this.logger.log(`Each Subscriber: ${element}`)
             this.logger.log(`Each Tokem : ${element.follower.token}`)
-            await this.fcmService.sendToToken(element.follower.token, body)
+            let fcmToken = element.follower.fcmToken;
+            if(fcmToken != null) {
+                await this.fcmService.sendToToken(fcmToken, body)
+            }
         }
     }
 
